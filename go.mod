3dce7a4ab125d7d93029d4410542006e9ab9a574
mr2cef <mr2cef@gmail.com>
2021-07-29 23:23:17 +0200

    read data and write to dummydb

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/mr2cef/open_data_tyrol
 
 go 1.16
 
-require github.com/tobgu/qframe v0.3.6 // indirect
+require (
+	github.com/influxdata/influxdb-client-go/v2 v2.4.0 // indirect
+	github.com/tobgu/qframe v0.3.6 // indirect
+)
--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"fmt"
 	http "net/http"
+	"time"
 
 	"github.com/tobgu/qframe"
+	"github.com/tobgu/qframe/config/csv"
+
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
 )
 
 func main() {
@@ -12,9 +16,49 @@ func main() {
 	if err != nil {
 		fmt.Println(err.Error())
 	}
+	dtypes := map[string]string{
+		"Stationsname":           "string",
+		"Stationsnummer":         "string",
+		"Gew�sser":               "string",
+		"Parameter":              "string",
+		"Zeitstempel in ISO8601": "string",
+		"Wert":                   "float",
+		"Einheit":                "string",
+		"Seeh�he":                "float",
+		"Rechtswert":             "float",
+		"Hochwert":               "float",
+		"EPSG-Code":              "string",
+	}
 
-	f := qframe.ReadCSV(resp.Body)
-	fmt.Println(f)
-
+	df := qframe.ReadCSV(resp.Body, csv.Delimiter(';'), csv.Types(dtypes))
 	defer resp.Body.Close()
+
+	// Create a new client using an InfluxDB server base URL and an authentication token
+	// and set batch size to 20
+	client := influxdb2.NewClientWithOptions("http://localhost:8086", "my-token",
+		influxdb2.DefaultOptions().SetBatchSize(20))
+	// Get non-blocking write client
+	writeAPI := client.WriteAPI("my-org", "my-bucket")
+
+	value := df.MustFloatView("Wert")
+	datetime, _ := df.StringView("Zeitstempel in ISO8601")
+	for i := 0; i < df.Len(); i++ {
+		layout := "2006-01-02T15:04:05-0700"
+		t, _ := time.Parse(layout, *datetime.ItemAt(i))
+		p := influxdb2.NewPoint(
+			"system",
+			map[string]string{
+				"id": "hello",
+			},
+			map[string]interface{}{
+				"value": value.ItemAt(i),
+			},
+			t)
+		fmt.Println(p)
+	}
+	// Force all unwritten data to be sent
+	writeAPI.Flush()
+	// Ensures background processes finishes
+	client.Close()
+
 }
