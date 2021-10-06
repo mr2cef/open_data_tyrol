41f71bb60f3eb2693b0cb59808f30abf704914e8
mr2cef <mr2cef@gmail.com>
2021-10-06 10:27:06 +0200

    make mongo a bit more pretty

--- mongo/mongo.go
+++ mongo/mongo.go
@@ -3,6 +3,7 @@ package mongo
 import (
 	"context"
 	"log"
+	"os"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
@@ -21,47 +22,56 @@ func isInList(val string, slice []string) (int, bool) {
 	return -1, false
 }
 
-func WriteDB(stationc chan map[string]string, donec chan string) {
+func mongoGetColletion() *mongo.Collection {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://127.0.0.1:27017"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGO_DB_HOST")))
 	if err != nil {
 		log.Println(err)
 	}
-	defer func() {
-		if err = client.Disconnect(ctx); err != nil {
-			panic(err)
-		}
-	}()
 
 	ctx, cancel = context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
 	if err = client.Ping(ctx, readpref.Primary()); err != nil {
 		panic(err)
 	}
-	collection := client.Database("OpenData").Collection("stations")
+	c := client.Database(os.Getenv("MONGO_DB_DB")).Collection(os.Getenv("MONGO_DB_COLLECTION"))
+	return c
+}
+
+func mongoWriteToCollection(c *mongo.Collection, m map[string]string) {
+	b, err := bson.Marshal(m)
+	if err != nil {
+		log.Println("Cannot marshal map.")
+	}
+	o := options.Replace()
+	o.SetUpsert(true)
+	_, err = c.ReplaceOne(
+		context.Background(),
+		bson.D{primitive.E{Key: "_id", Value: m["_id"]}},
+		b,
+		o,
+	)
+	if err != nil {
+		log.Printf("Cannot write to DB: %+v\n", err)
+	}
+}
+
+func WriteDB(stationc chan map[string]string, donec chan string) {
+
+	c := mongoGetColletion()
+	defer func() {
+		if err := c.Database().Client().Disconnect(context.Background()); err != nil {
+			panic(err)
+		}
+	}()
 	don := []string{}
 	for m := range stationc {
 		_, isIn := isInList(m["_id"], don)
 		if !isIn {
-			b, err := bson.Marshal(m)
-			if err != nil {
-				log.Println("Cannot marshal map.")
-			}
-			o := options.Replace()
-			o.SetUpsert(true)
-			_, err = collection.ReplaceOne(
-				context.Background(),
-				bson.D{primitive.E{Key: "_id", Value: m["_id"]}},
-				b,
-				o,
-			)
-			if err != nil {
-				log.Printf("%+v\n", err)
-			}
+			mongoWriteToCollection(c, m)
+			don = append(don, m["_id"])
 		}
-		don = append(don, m["_id"])
-
 	}
 	donec <- "Mongo is done.\n"
 }
